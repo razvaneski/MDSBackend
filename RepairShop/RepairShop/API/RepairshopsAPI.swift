887e149a18bc012cbe6159aafe05c31e99609410
Murica Andrei <andreimurica@gmail.com>
2023-06-04 22:10:04 +0300

    get/add review API

--- RepairShop/RepairShop/API/RepairshopsAPI.swift
+++ RepairShop/RepairShop/API/RepairshopsAPI.swift
@@ -34,4 +34,31 @@ class RepairshopsAPI: BaseAPI {
             }
         }
     }
+    
+    func getReviews(token: String, repairshopId: String) -> Single<[Review]> {
+        let headers = ["token": token]
+        
+        return call(endpoint: "getreviews?repairshop_id=\(repairshopId)", method: .get, headers: headers) { json in
+            return json.arrayValue.map { reviewJson in
+                return Review(
+                    userId: reviewJson["user_id"].stringValue,
+                    repairshopId: reviewJson["repairshop_id"].stringValue,
+                    rating: reviewJson["rating"].intValue,
+                    message: reviewJson["message"].stringValue
+                )
+            }
+        }
+    }
+    
+    func addReview(token: String, review: Review) -> Completable {
+        let headers = ["token": token]
+        let params = [
+            "rating": String(review.rating),
+            "message": review.message
+        ]
+        
+        return call(endpoint: "addReview?repairshop_id=\(review.repairshopId)", method: .post, params: params, headers: headers) { _ in
+            //
+        }.asCompletable()
+    }
 }
